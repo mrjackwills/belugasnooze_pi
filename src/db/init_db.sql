3afabbe00276bdd7ca3b7be2e79a901a8bfa6abc
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-01-27 09:08:52 +0000

    chore: sql file formatted

--- src/db/init_db.sql
+++ src/db/init_db.sql
@@ -1,13 +1,23 @@
 BEGIN;
- 	CREATE TABLE IF NOT EXISTS alarm  (
+
+CREATE TABLE IF NOT EXISTS alarm (
 	alarm_id INTEGER PRIMARY KEY AUTOINCREMENT,
-	day INTEGER NOT NULL CHECK (day >= 0 AND day <= 6),
-	hour INTEGER NOT NULL CHECK (hour >= 0 AND hour <= 23),
-	minute INTEGER NOT NULL CHECK (minute >= 0 AND minute <= 59),
+	day INTEGER NOT NULL CHECK (
+		day >= 0
+		AND day <= 6
+	),
+	hour INTEGER NOT NULL CHECK (
+		hour >= 0
+		AND hour <= 23
+	),
+	minute INTEGER NOT NULL CHECK (
+		minute >= 0
+		AND minute <= 59
+	),
 	UNIQUE (day, hour, minute)
 ) STRICT;
 
-CREATE TABLE IF NOT EXISTS timezone  (
+CREATE TABLE IF NOT EXISTS timezone (
 	timezone_id INTEGER PRIMARY KEY AUTOINCREMENT CHECK (timezone_id = 1),
 	zone_name TEXT NOT NULL
 ) STRICT;
